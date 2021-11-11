eccc6fe8485e3f027ad86df9edd04738977073d1
William Fondrie <fondriew@gmail.com>
2021-11-10 16:20:20 -0800

    First attempt

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:18.04
+
+RUN apt-get update && \
+    apt-get install procps -y && \
+    apt-get clean
+
+COPY comet.exe comet
---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+version="${VERSION:-HEAD}"
+
+checkout () {
+    git clone https://github.com/UWPR/Comet.git
+    git checkout ${version}
+    cd Comet
+}
+
+main () {
+    checkout
+    make
+    mv comet.exe ../comet.exe
+}
+
+main
